af2220977fbd87de110bb6c2a203fc7a17ea25d0
Josh Komusin <jkomusin@gmail.com>
2012-03-27 14:28:22 -0400

    Updated documentation of PortfolioController with PHPDoc

--- controllers/portfolio.php
+++ controllers/portfolio.php
@@ -1,42 +1,34 @@
 <?php
-require_once('libraries/Idiorm/idiorm.php');
-require_once('libraries/Paris/paris.php');
+//require_once('libraries/Idiorm/idiorm.php');
+//require_once('libraries/Paris/paris.php');
 
 //ORM::configure('mysql:host=kenai.asap.um.maine.edu;dbname=mainejournal_dev');
 //ORM::configure('username', 'root');
 
 
 /**
- *	Portfolio object
+ * Controller handling all aspects of Portfolio objects within the system.
  *
- *	A Portfolio object represents a single row in the REPO_Portfolios table, along with
- *	all associated data derived from its relations.
- *
- *	Information retrieved through relations:
- *	- permissions: specified based on the requesting user. Return value corresponds
- *	  to an enumerable value as specified in 'constants.php'.
- *	- children: any children Portfolio or Project objects underneath the current one.
- *		'children' object is an array of tuples consisting of:
- *		- identifier of child Portfolio / Project object.
- *		- boolean value specifying whether the child is a sub-Portfolio or Project.
- *
- *********************************************************************************************/
-
-
+ * @package Controllers
+ */
 class PortfolioController
 {
 	/**
-	 *	Create a new Portfolio object with the specified parameters and the current
-	 *	user as the owner.
-	 *	- 'title' is a string specifying the title of the Portfolio (255 char max).
-	 *	- 'description' is a plain-text string describing the Portfolio (2^16 char max).
-	 *	- 'private' is a boolean specifying whether or not the Portfolio is considered private
-	 *	  (true = private, false = public).
-	 *	Returns: the created Portfolio object if successful, false otherwise.
+	 *	Create a new Portfolio object.
 	 *
-	 *********************************************************************************************/
+	 *	Creates a new Portfolio object in the system with the specified parameters. The currently
+	 *	logged in user is given 'owner' level permissions.
+	 *
+	 *	@param	string	$title			Specifies the title of the new Portfolio in plain-text (255 char max).
+	 *	@param	string	$description 	Describes the new Portfolio in plain-text (2^16 char max).
+	 *	@param	bool	$private		Specifies whether or not the Portfolio is considered private
+	 *									(true = private, false = public).
+	 *
+	 *	@return	object|bool				The created Portfolio object if successful, false otherwise.
+	 */
 	static function createPortfolio($title, $description, $private)
 	{
+		//TODO: check privileges here
 		$port = Model::factory('Portfolio') -> create();
 		$port->title = $title;
 		$port->description = $description;
@@ -48,16 +40,19 @@ class PortfolioController
 
 
 	/**
-	 *	Edit a specific Portfolio object that the current user has 'editing' (atleast)
-	 *	privileges for.
-	 *	- 'id' is the unique identifier of the Portfolio being edited.
-	 *	- 'title' is a string specifying the title of the Portfolio (255 char max).
-	 *	- 'description' is a plain-text string describing the Portfolio (2^16 char max).
-	 *	- 'private' is a boolean specifying whether or not the Portfolio is considered private
-	 *	  (true = private, false = public).
-	 *	Returns: true if successfully edited, false otherwise.
+	 *	Edit a specific Portfolio object.
 	 *
-	 *********************************************************************************************/
+	 *	Edits paramaters of a Portfolio object in the system that the current user has 'editing' (atleast)
+	 *	privileges for.
+	 *
+	 *	@param	int		$id				The unique identifier of the Portfolio object being edited.
+	 *	@param	string	$title			The title of the Portfolio to be set, in plain-text (255 char max).
+	 *	@param	string	$description	The description of the Portfolio to be set, in plain-text (2^16 char max).
+	 *	@param	bool	$private	 	Specifies whether or not the Portfolio is considered private
+	 *									(true = private, false = public).
+	 *
+	 *	@return	bool					True if successfully edited, false otherwise.
+	 */
 	static function editPortfolio($id, $title, $description, $private)
 	{
 		//TODO: check privileges here
@@ -79,12 +74,15 @@ class PortfolioController
 
 
 	/**
-	 *	Delete a specific Portfolio object that the current user owns.
-	 *	NOTE: Does not delete Projects or sub-Portfolios.
-	 *	- 'id' is the unique identifier of the Portfolio to be deleted.
-	 *	Returns: true if successfully deleted, false otherwise.
+	 *	Delete a specific Portfolio object.
 	 *
-	 *********************************************************************************************/
+	 *	Deletes a Portfolio object in the system that the current user owns.
+	 *	Does _not_ delete Projects or sub-Portfolios.
+	 *
+	 *	@parm	int		$id				The unique identifier of the Portfolio to be deleted.
+	 *
+	 *	@return	bool					True if successfully deleted, false otherwise.
+	 */
 	static function deletePortfolio($id)
 	{
 		$port = Model::factory('Portfolio')
@@ -102,28 +100,33 @@ class PortfolioController
 
 	/**
 	 *	Retrieve a specific Portfolio object.
-	 *	- 'id' is the specified identifier of the Portfolio to be retrieved.
-	 *	Returns: the Portfolio object requested if successful, false otherwise.
 	 *
-	 *********************************************************************************************/
+	 *	Retrieves a Portfolio object, constructed as specified within the Portfolio model object description.
+	 *
+	 *	@param	int		$id				The specified identifier of the Portfolio to be retrieved.
+	 *
+	 *	@return	object|bool				The Portfolio object requested if successful, false otherwise.
+	 */
 	 static function getPortfolio($id)
 	 {
 	 	return Model::factory('Portfolio')
-	 		-> find_one($id);
+			-> find_one($id);
 	 }
 
 
 	/**
-	 *	Retrieve a specified number of Portfolio objects the current user is a member of.
-	 *	A User is a 'member' of a Portfolio if they have any privileges specific to them
-	 *	(i.e. higher than public-level access).
-	 *	- 'count' is the number of Portfolio objects desired.
-	 *	- 'order_by' is the field that the Portfolios should be ordered by (ex: date_descending),
-	 *	  as specified in 'constants.php'.
-	 *	- 'pos' is the index of the first Portfolio to return in the set 'order_by' specifies.
-	 *	Returns: an array of Portfolio objects if successful, false otherwise.
+	 *	Retrieve a number of Portfolio objects the current user is a member of.
 	 *
-	 *********************************************************************************************/
+	 *	Retrieves a specified number of Portfolio objects. A User is a 'member' of a Portfolio if they 
+	 *	have any privileges specific to them (i.e. higher than public-level access).
+	 *
+	 *	@param	int		$count			The number of Portfolio objects desired.
+	 *	@param	int		$order_by		The field that the Portfolios should be ordered by (ex: date_descending),
+	 *									as specified in 'constants.php'.
+	 *	@param	int		$pos			The index of the first Portfolio to return in the set 'order_by' specifies.
+	 *
+	 *	@return	array|bool				An array of Portfolio objects if successful, false otherwise.
+	 */
 	 static function getMemberPortfolios($count, $order_by, $pos)
 	 {
 		 return false;
@@ -131,16 +134,18 @@ class PortfolioController
 
 
 	/**
-	 *	Retrieve a specified number of Portfolio objects the current User is included in.
-	 *	A User is 'included in' a Portfolio if they have any Projects they are a 'member' of
-	 *	included in the Portfolio or its sub-Portfolios.
-	 *	- 'count' is the number of Portfolio objects desired (0 = return all such Portfolios).
-	 *	- 'order_by' is the field that the Portfolios should be ordered by (ex: date_descending),
-	 *	  as specified in 'constants.php'.
-	 *	- 'pos' is the index of the first Portfolio to return in the set 'order_by' specifies.
-	 *	Returns: an array of Portfolio objects if successful, false otherwise.
+	 *	Retrieve a number of Portfolio objects the current User is included in.
 	 *
-	 *********************************************************************************************/
+	 *	Retrieves a specified number of Portfolio objects. A User is 'included in' a Portfolio if they
+	 *	have any Projects they are a 'member' of included in the Portfolio or its sub-Portfolios.
+	 *
+	 *	@param	int		$count			The number of Portfolio objects desired (0 = return all such Portfolios).
+	 *	@param	int		$order_by		The field that the Portfolios should be ordered by (ex: date_descending),
+	 *									as specified in 'constants.php'.
+	 *	@param	int		$pos			The index of the first Portfolio to return in the set 'order_by' specifies.
+	 *
+	 *	@return	array|bool				An array of Portfolio objects if successful, false otherwise.
+	 */
 	 static function getIncludedPortfolios($count, $order_by, $pos)
 	 {
 		 return false;
@@ -148,14 +153,18 @@ class PortfolioController
 
 
 	/**
-	 *	Retrieve a specified number of public Portfolio objects.
-	 *	- 'count' is the number of Portfolio objects desired (0 = return all such Portfolios).
-	 *	- 'order_by' is the field that the Portfolios should be ordered by (ex: date_descending),
-	 *	  as specified in 'constants.php'.
-	 *	- 'pos' is the index of the first Portfolio to return in the set 'order_by' specifies.
-	 *	Returns: an array of Portfolio objects if successful, false otherwise.
+	 *	Retrieve a number of public Portfolio objects.
 	 *
-	 *********************************************************************************************/
+	 *	Retrieves a specified number of Portfolio objects. Public Portfolios are Portfolio objects
+	 *	with their 'private' property set to false.
+	 *
+	 *	@param	int		$count			The number of Portfolio objects desired (0 = return all such Portfolios).
+	 *	@param	int		$order_by		The field that the Portfolios should be ordered by (ex: date_descending),
+	 *									as specified in 'constants.php'.
+	 *	@param	int		$pos			The index of the first Portfolio to return in the set 'order_by' specifies.
+	 *
+	 *	@return	array|bool				An array of Portfolio objects if successful, false otherwise.
+	 */
 	 static function getPublicPortfolios($count, $order_by, $pos)
 	 {
 		 return false;
@@ -163,13 +172,16 @@ class PortfolioController
 
 
 	/**
-	 *	Add a specific Portfolio as a sub-Porfolio of a specified Portfolio.
-	 *	NOTE: Must take care to prevent circular references!
-	 *	- 'parent' is the identifier of the parent Portfolio object.
-	 *	- 'child' is the identifier of the child sub-Portfolio object.
-	 *	Returns: true if successful, false otherwise.
+	 *	Add a Portfolio as a sub-Porfolio of another Portfolio.
 	 *
-	 *********************************************************************************************/
+	 *	Adds a specific Portfolio as a 'child' to another Portfolio as its 'parent'.
+	 *	In the event of an attempt at a circular reference, the method will return false.
+	 *
+	 *	@param	int		$parent			The identifier of the parent Portfolio object.
+	 *	@param	int		$child			The identifier of the child sub-Portfolio object.
+	 *
+	 *	@return	bool					True if successful, false otherwise.
+	 */
 	 static function addSubPortfolio($parent, $child)
 	 {
 	 	if ($parent == $child)
@@ -194,12 +206,15 @@ class PortfolioController
 
 
 	/**
-	 *	Add a specific Project object to a specific Portfolio object.
-	 *	- 'parent' is the identifier of the parent Portfolio object.
-	 *	- 'child' is the identifier of the child Project object.
-	 *	Returns: true if successful, false otherwise.
+	 *	Add a Project object to a Portfolio object.
 	 *
-	 *********************************************************************************************/
+	 *	Adds a specific Project object as a 'child' to a Portfolio object as its 'parent'.
+	 *	
+	 *	@param	int		$parent			The identifier of the parent Portfolio object.
+	 *	@param	int		$child			The identifier of the child Project object.
+	 *
+	 *	@return	bool					True if successful, false otherwise.
+	 */
 	 static function addProjectToPortfolio($parent, $child)
 	 {
 	 	$project = ProjectController::getProject($child);
@@ -221,14 +236,17 @@ class PortfolioController
 
 
 	/**
-	 *	Remove a specific child object from a specific Portfolio object.
-	 *	- 'parent' is the identifier of the parent Portfolio object.
-	 *	- 'child' is the identifier of the child object.
-	 *	- 'isSubPortfolio' is a boolean indicating whether a child is a sub-Portfolio or not
-	 *	  (true = child is sub-Portfolio, false = child is not sub-Portfolio).
-	 *	Returns: true if successful, false otherwise.
+	 *	Remove a 'child' object from a Portfolio object.
 	 *
-	 *********************************************************************************************/
+	 *	Removes a 'child' object (a Project or sub-Portfolio) from its 'parent' object (a Portfolio).
+	 *	
+	 *	@param	int		$parent			The identifier of the parent Portfolio object.
+	 *	@param	int		$child			The identifier of the child object.
+	 *	@param	bool	$isSubPortfolio	A bool indicating whether a child is a sub-Portfolio or not
+	 *									(true = child is sub-Portfolio, false = child is not sub-Portfolio).
+	 *
+	 *	@return	bool					True if successful, false otherwise.
+	 */
 	 static function removeChildFromPortfolio($parent, $child, $isSubPortfolio)
 	 {
 		 return false;
@@ -236,13 +254,17 @@ class PortfolioController
 
 
 	/**
-	 *	Gets permissions for a specific Group object in regards to a specific Portfolio object.
-	 *	- 'portfolio' is the identifier of the Portfolio the Group is being assigned to.
-	 *	- 'group' is the Group object recieving permissions for the Portfolio.
-	 *	Returns: an array of all permissions the specific Group object has on the specific
-	 *	Portfolio object, as specified in 'constants.php'. If no permissions, returns empty array.
+	 *	Retrieve permissions for a Group object in regards to a Portfolio object.
 	 *
-	 *********************************************************************************************/
+	 *	Gets all permissions set for a Portfolio object for a specific Group. 
+	 *
+	 *	@param	int		$portfolio		The identifier of the Portfolio the Group is being assigned to.
+	 *	@param	int		$group			The identifier of the Group object recieving permissions for the Portfolio.
+	 *
+	 *	@return	array					An array of all permissions the specific Group object has on the specific
+	 *									Portfolio object, as specified in 'constants.php'. 
+	 *									If no permissions, returns empty array.
+	 */
 	 static function getPortfolioPermissionsForGroup($portfolio, $group)
 	 {
 		 return array();
@@ -250,17 +272,19 @@ class PortfolioController
 
 
 	/**
-	 *	Adds a specific permission to a specific Group object in regards to a specific Portfolio 
-	 *	object.
-	 *	NOTE: Permission cascades down the Portfolio tree (i.e., the Group will have permission for
-	 *	all sub-Portfolios as well).
-	 *	- 'portfolio' is the identifier of the Portfolio the Group is being assigned to.
-	 *	- 'group' is the Group object recieving permissions for the Portfolio.
-	 *	- 'permission' is the type of permission being granted to the Group object,
-	 *	  as specified in 'constants.php'.
-	 *	Returns: true if successful, false otherwise.
+	 *	Add a permission level to a Group in regards to a Portfolio.
 	 *
-	 *********************************************************************************************/
+	 *	Appends a specified permission level to the Group's current list of permissions.
+	 *	The appended permission cascades down the Portfolio tree (i.e., the Group will have permission for
+	 *	all sub-Portfolios as well).
+	 *
+	 *	@param	int		$portfolio		The identifier of the Portfolio the Group is being assigned to.
+	 *	@param	int		$group			The identifier of the Group object recieving permissions for the Portfolio.
+	 *	@param	int		$permission		The type of permission being granted to the Group object,
+	 *	  								as specified in 'constants.php'.
+	 *
+	 *	@return	bool					True if successful, false otherwise.
+	 */
 	 static function addPortfolioPermissionsForGroup($portfolio, $group, $permission)
 	 {
 		return false;
@@ -268,17 +292,18 @@ class PortfolioController
 
 
 	/**
-	 *	Removes a specific permission from a specific Group object in regards to a specific Porfolio
-	 *	object.
-	 *	NOTE: Permission cascades down the Portfolio tree (i.e., the Group will have permission
-	 *	revoked for	all sub-Portfolios as well).
-	 *	- 'portfolio' is the identifier of the Portfolio the Group's permissions are revoked from.
-	 *	- 'group' is the Group object losing permissions for the Portfolio.
-	 *	- 'permission' is the type of permission being removed from the Group object,
-	 *		as specified in 'constants.php'.
-	 *	Returns: true if successful, false otherwise.
+	 *	Remove a permission from a Group in regards to a Porfolio object.
+	 *	
+	 *	Removes a permission from a Group object associated with a Portfolio. The permission cascades 
+	 *	down the Portfolio tree (i.e., the Group will have the permission revoked for all sub-Portfolios as well).
 	 *
-	 *********************************************************************************************/
+	 *	@param	int		$portfolio		The identifier of the Portfolio the Group's permissions are revoked from.
+	 *	@param	int		$group			The identifier of the Group object losing permissions for the Portfolio.
+	 *	@param	int		$permission		The type of permission being removed from the Group object,
+	 *									as specified in 'constants.php'.
+	 *
+	 *	@return	bool					True if successful, false otherwise.
+	 */
 	 static function removePortfolioPermissionsForGroup($portfolio, $group, $permission)
 	 {
 		 return false;
