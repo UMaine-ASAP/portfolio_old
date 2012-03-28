bf288d67d88d928d6da8c4aae2be21d850418bbf
Josh Komusin <jkomusin@gmail.com>
2012-03-28 12:03:45 -0400

    Ive made a horrible mistake, mappings are back in (after renaming)

---
+++ models/mappings.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class ConnectionProjectMap extends Model
+{
+	public static $_table = "REPO_Collection_project_map";
+	public static $_id_column = "collect_id";
+}
+
+
+class ProjectMediaMap extends Model
+{
+	public static $_table = 'REPO_Project_media_map';
+	public static $_id = 'proj_id';
+}
+
+
+?>
