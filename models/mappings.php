47f823b35a5e3ed8c90bc21bc318a1d469deb924
Josh Komusin <jkomusin@gmail.com>
2012-03-27 20:08:52 -0400

    Removed individual mapping model files for an all-inclusive mappings.php file. Mapping models are minimally complex, and often used together.

--- models/mappings.php
+++ models/mappings.php
@@ -1,9 +1,18 @@
 <?php
 
+
 class collection_project_map extends Model
 {
 	public static $_table = "Collection_project_map";
 	public static $_id_column = "collect_id";
 }
+
+
+class project_media_map extends Model
+{
+	public static $_table = 'Project_media_map';
+	public static $_id = 'proj_id';
+}
+
 
 ?>
