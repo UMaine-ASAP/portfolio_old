c1dd7d4af06e3842a0b0404fe4fe591ce5a98c29
Josh Komusin <jkomusin@gmail.com>
2012-03-27 20:06:26 -0400

    Added libraries/constant.php file to store global constants.

---
+++ libraries/constant.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Library of global constants in the system.
+ *
+ * @package Constants
+ */
+
+/**
+ * Permission types.
+ *
+ * Constant mapping of integer values to standard permission names
+ */
+/**
+ * OWNER
+ *
+ * Has overarching total permission of an object (read, write, edit, delete)
+ */
+DEFINE("OWNER",	0);
+/**
+ * WRITE
+ *
+ * Has ability to write to or add to an object (i.e. can add to a Portfolio).
+ */
+DEFINE("WRITE", 1);
+/**
+ * EDIT
+ *
+ * Has the ability to write to an object's properties, but not add additional objects
+ * (i.e. cannot add to Portfolio, Group, etc. but can modify name, etc.).
+ */
+DEFINE("EDIT", 	2);
+/**
+ * READ
+ *
+ * Has the ability to read an object's values only.
+ */
+DEFINE("READ",	3);
+
+?>
