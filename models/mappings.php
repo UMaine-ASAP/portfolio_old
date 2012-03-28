02272fd84a8e843b934404e018faff5eb693891a
Ross Trundy <ross.trundy@maine.edu>
2012-03-28 15:58:19 -0400

    Added functionality to the assignment model.

--- models/mappings.php
+++ models/mappings.php
@@ -9,7 +9,6 @@ class ConnectionProjectMap extends Model
 	public static $_id_column = "collect_id";
 }
 
-
 class ProjectMediaMap extends Model
 {
 	public static $_table = 'REPO_Project_media_map';
