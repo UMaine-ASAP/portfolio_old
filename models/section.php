31d3293020044ba7b17974c53cebdd9bb9f7cc7c
Ross Trundy <ross.trundy@maine.edu>
2012-04-04 14:23:45 -0400

    Fleshed out Section model

--- models/section.php
+++ models/section.php
@@ -4,6 +4,34 @@ class Section extends Model
 {
 	public static $_table = "REPO_Sections";
 	public static $_id_column = "section_id";
+
+	public function days()
+	{
+		return ORM::for_table('REPO_Day_schedules')
+					->where('sched_id', $this->day_sched)
+					->find_one();
+	}
+
+	public function ownerGroup()
+	{
+		return Model::factory('Group')
+					->where('group_id', $this->owner_id)
+					->find_one();
+	}
+
+	public function classes()
+	{
+		return Model::factory('ClassModel')
+					->where('class_id', $this->class_id)
+					->find_many();
+	}
+
+	public function owner()
+	{
+		return Model::factory('User')
+					->where('user_id', $this->owner_id)
+					->find_one();
+	}
 }
 
 ?>
