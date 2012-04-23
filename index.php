423f74fefe4564dbf51ebfb8d9aeb2ba5d765c62
Josh Komusin <jkomusin@gmail.com>
2012-04-23 01:55:24 -0400

    Truncation of long descriptions and titles of Projects

--- index.php
+++ index.php
@@ -63,6 +63,21 @@ function permission_denied()
 	$GLOBALS['app']->render('permission_denied.html');
 }
 
+function string_truncate($string, $desired_length)
+{
+  $parts = preg_split('/([\s\n\r]+)/', $string, null, PREG_SPLIT_DELIM_CAPTURE);
+  $parts_count = count($parts);
+
+  $length = 0;
+  $last_part = 0;
+  for (; $last_part < $parts_count; ++$last_part) 
+  {
+    $length += strlen($parts[$last_part]);
+    if ($length > $desired_length) { break; }
+  }
+
+  return implode(array_slice($parts, 0, $last_part));
+}
 
 /************************************************
  * ROUTING!!									*
@@ -219,7 +234,13 @@ $app->get('/portfolio', $authcheck_student, function() use ($app) {
 		foreach ($nmd_port->children as $child_id=>$arr)
 		{
 			$proj = ProjectController::viewProject($child_id);	// assume all children are Projects
-			$projects[] = array("project_id" => $proj->id(), "title" => $proj->title, "description" => $proj->description, "type" => $proj->type);
+			// Trim title if it is too long
+			$t = string_truncate($proj->description, 50);
+			if (strlen($t) < strlen($proj->title)) { $t = $t . "..."; }
+			// Trim description if it is too long
+			$desc = string_truncate($proj->description, 500);
+			if (strlen($desc) < strlen($proj->description)) { $desc = $desc . "..."; }
+			$projects[] = array("project_id" => $proj->id(), "title" => $t, "description" => $desc, "type" => $proj->type);
 		}
 	}
 		
@@ -286,6 +307,7 @@ $app->get('/project/:id/edit', $authcheck_student, function($id) use ($app) {
 	}
 	else
 	{
+		$media = array();
 		foreach ($proj->media as $media_id)
 		{
 			$m = MediaController::viewMedia($media_id);
