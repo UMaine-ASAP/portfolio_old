70d0f5b95aca971295ac02cc26bb21fa989049a4
root <root@kenai.asap.um.maine.edu>
2012-04-25 09:45:42 -0400

    Changes to Slim and settings to production

--- libraries/settings.php
+++ libraries/settings.php
@@ -6,14 +6,14 @@ if( $IS_PRODUCTION ) {
 
 } else {
 	$HOST = "localhost";
-	$DATABASE = "mj_dev";
-	$USERNAME = "asap";
-	$PASSWORD = "asap4u";
+	$DATABASE = "nmp_dev_tim";
+	$USERNAME = "root";
+	$PASSWORD = "asap4u2u";
 }
 
 /** Time until idle sessions are disconnected */
 $session_timeout = 600; // 10 minutes to logout
-
+$thumbnail_path = "/media/thumbnails/";
 
 /** Web Root */
-$web_root = "/mainejournal";
+$web_root = "/portfolio";
