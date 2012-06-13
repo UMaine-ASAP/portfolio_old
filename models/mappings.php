58fe5ae90ff2d13cd3b9e825895c5948663cd3d5
Ross Trundy <ross.trundy@maine.edu>
2012-06-13 15:05:05 -0400

    MAPPINGS

--- models/mappings.php
+++ models/mappings.php
@@ -49,7 +49,7 @@ class SectionAccessMap extends Model
 				return $this->section_id;
 				break;
 
-			case 'section'
+			case 'section':
 				return Model::factory('Section')->find_one($this->section_id);
 				break;
 
