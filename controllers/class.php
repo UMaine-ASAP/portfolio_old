9175e3a048ef5e69686c6472af7eca16351d4fce
Ross Trundy <ross.trundy@maine.edu>
2012-08-10 12:17:02 -0400

    Added some functionality to class controller

--- controllers/class.php
+++ controllers/class.php
@@ -12,11 +12,40 @@ require_once('models/class.php');
  */
 class ClassController
 {
-	public static function getClass($id)
-	{
-		return Model::factory('ClassModel')->find_one($id);
-	}
+    /**
+     * Returns a Class ORM object with the specified ID, or false if none were found
+     * @param int $id The ID of the class 
+     * @return The class ORM object, or false if no classes with $id exist
+     */
+    public static function getClass($id)
+    {
+        return Model::factory('ClassModel')->find_one($id);
+    }
+    
+    /**
+     * Retrieves a list of assignments belonging to the class
+     * @param int $id The ID of the class 
+     * @return An array containing Assignment ORM objects, or false if no assignments were found
+     */
+    public function getClassAssignments($id)
+    {
+        if (!$class = getClass($id))
+        {
+            return false; //there's no such class
+        }
+        
+        $assignments = Model::factory('Assignment')
+                ->where('class_id', $class->id)
+                ->find_many();
+        
+        if (count($assignments) == 0)
+        {
+            return false;
+        }
+        
+        return $assignments;
+    }
+    
 }
-
 
 ?>
