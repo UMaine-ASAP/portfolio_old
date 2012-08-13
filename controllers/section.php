96ecdcb3c5fd2b414de163c5eec82ea357c6d927
Ross Trundy <ross.trundy@maine.edu>
2012-08-13 11:44:19 -0400

    More portfolio work

--- controllers/section.php
+++ controllers/section.php
@@ -2,6 +2,8 @@
 
 require_once('libraries/Idiorm/idiorm.php');
 require_once('libraries/Paris/paris.php');
+require_once('controllers/authentication.php');
+require_once('controllers/class.php');
 require_once('models/section.php');
 
 /**
@@ -22,7 +24,7 @@ class SectionController
     }
     
     /**
-     *
+     * Creates a section with the specified parameters and saves it to the database.
      * @param type $classID
      * @param type $sectionNumber
      * @param type $daySched
@@ -32,10 +34,49 @@ class SectionController
      * @param type $year
      * @param type $designator
      * @param type $description 
+     * 
+     * @return True if the section creation succeeded, false otherwise 
      */
     public function createSection($classID, $sectionNumber, $daySched, $time, $instructorID, $semester, $year, $designator, $description)
     {
+        if (!$userID = AuthenticationController::getCurrentUserID())
+        {
+            return false;
+        }
         
+        if (is_null($sectionNumber) || is_null($daySched) || is_null($time) || is_null($instructorID) || is_null($semester) || is_null($year) || is_null($designator) || is_null($description))
+        {
+            error_log("Parameters passed to createSection must not be null");
+            return false;
+        }
+        
+        if (!$class = ClassController::getClass($classID))
+        {
+            error_log("Class $classID does not exist");
+            return false;
+        }
+        
+        $section = Model::factory('Section')->create();
+        
+        $section->section_number = $sectionNumber;
+        $section->day_sched = $daySched;
+        $section->time = $time;
+        $section->instructor_user_id = $instructorID;
+        $section->semester = $semester;
+        $section->year = $year;
+        $section->designator = $designator;
+        $section->description = $description;
+        
+        $result = $section->save();
+        
+        if ($result)
+        {
+            //if saving succeeded
+            $section->addPermissionForUser($userID, OWNER);
+            return $result;
+        }
+        
+        return $result;
     }
     
     /**
--- models/class.php
+++ models/class.php
@@ -2,48 +2,60 @@
 
 class ClassModel extends Model
 {
-	public static $_table = 'REPO_Classes';
-	public static $_id_column = 'class_id';
-        
-            
-        /**
-        * Retrieves a list of assignments belonging to the class
-        * @return An array containing Assignment ORM objects, or false if no assignments were found
-        */
-        public function assignments()
-        {
-            return Model::factory('Assignment')
-                    ->where('class_id', $this->id)
-                    ->find_many();
 
-        }
-        
-        /**
-         * Retrieves all instructors who teach this class
-         * @return An array containing User ORM objects, or false if no instructors were found for this class
-         */
-        public function instructors()
+    public static $_table = 'REPO_Classes';
+    public static $_id_column = 'class_id';
+
+    /**
+     * Retrieves a list of assignments belonging to the class
+     * @return An array containing Assignment ORM objects, or false if no assignments were found
+     */
+    public function assignments()
+    {
+        return Model::factory('Assignment')
+                        ->where('class_id', $this->id)
+                        ->find_many();
+    }
+
+    /**
+     * Retrieves all instructors who teach this class
+     * @return An array containing User ORM objects, or false if no instructors were found for this class
+     */
+    public function instructors()
+    {
+        $sections = Model::factory('Section')
+                ->where('class_id', $this->id)
+                ->find_many();
+
+        $instructors = array();
+
+        //iterate over each section, then add each section's instructor to the list of instructors
+        foreach ($sections as $section)
         {
-            $sections = Model::factory('section')
-                    ->where('class_id', $this->id)
-                    ->find_many();
-            
-            $instructors = array();
-            
-            foreach($sections as $section)
-            {
-                $instructors[] = Model::factory('User')
-                        ->where('user_id', $section->instructor_user_id)
-                        ->find_one();
-            }
-            
-            if (count($instructors) == 0)
-            {
-                return false;
-            }
-            
-            return $instructors;
+            $instructors[] = Model::factory('User')
+                    ->where('user_id', $section->instructor_user_id)
+                    ->find_one();
         }
+
+        if (count($instructors) == 0)
+        {
+            return false;
+        }
+
+        return $instructors;
+    }
+    
+    /**
+     * Retrieves all sections of this class
+     * @return An array of ORM objects, or false if no sections were found
+     */
+    public function sections()
+    {
+        return Model::factory('Section')
+                ->where('class_id', $this->id)
+                ->find_many();
+    }
+
 }
 
 ?>
